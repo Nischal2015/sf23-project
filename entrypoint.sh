f6a6ecf16a34bc9e3d802023fc8e6cb543cdd873
Nischal Shakya <nischalshakya2015@gmail.com>
2022-07-23 00:33:03 +0545

    add comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 docker compose down -v
 
+# for destroying any volume
 docker compose up --build -d
 docker compose exec web python manage.py migrate --noinput
 docker compose exec web python manage.py collectstatic --no-input --clear
